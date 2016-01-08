d64f6be2f5a173921192a699f9b37d39a277bf5c
wongming <byonecry@qq.com>
2016-01-08 11:22:42 +0800

    add conf file commands in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 from setuptools import setup, find_packages
+import os,sys
+import shutil
+import traceback
+
 setup(
-    name = "stest",
+    name = '.stest',
     version = "1.0.0",
     packages = find_packages(),
     package_data = {'': ['*.txt',
@@ -14,3 +18,20 @@ setup(
         ]
     }
  )
+try:
+    conf_path = os.path.expanduser('~/.stest')
+    if not os.path.exists(conf_path):
+        os.mkdir(conf_path)
+    driver_path = os.path.join(conf_path, 'driver')
+    if not os.path.exists(driver_path):
+        os.mkdir(driver_path)
+    conf_file = os.path.join(conf_path,'stest.conf')
+    if not os.path.exists(conf_file):
+        shutil.copyfile('./stest/stest.conf.template', conf_file)
+    email_template_file = os.path.join(conf_path, 'EmailTemplate.html')
+    if not os.path.exists(email_template_file):
+        shutil.copyfile('./stest/EmailTemplate.html',email_template_file)
+except:
+    print 'Conf resource processed error'
+    traceback.print_exc()
+    #print "Remove dir '~/.stest' and try again"
